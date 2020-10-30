94324a18a5e506777908e0da78772ba8e91fc279
Trofimov Nikita <trofimov1nv@gmail.com>
2020-10-30 22:00:36 +0300

    Init program

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int result = balance + transferAmount;
+
+        System.out.println(result);
+    }
+}
